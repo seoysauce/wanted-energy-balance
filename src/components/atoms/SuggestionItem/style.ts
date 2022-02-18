7ff31a6c08add7def00b4ca221492555a8ecb6f2
gml9812 <hee.jun.yun@gmail.com>
2022-02-18 23:02:44 +0900

    style: 선택 시 강조색 보이도록 수정

--- src/components/atoms/SuggestionItem/style.ts
+++ src/components/atoms/SuggestionItem/style.ts
@@ -2,33 +2,25 @@ import styled from 'styled-components';
 
 export const Container = styled.li<{ isActive: boolean }>`
   color: ${(props) => (props.isActive ? '#fc9700' : 'black')};
-  background: ${(props) => (props.isActive ? '#808080' : '#d3d3d3')};
+  background: ${(props) => (props.isActive ? '#d3d3d3' : '#white')};
   cursor: pointer;
   width: 100%;
   font-weight: 700;
   max-height: 100px;
-  background-color: white;
 `;
 
 export const Description = styled.div`
-  -webkit-text-size-adjust: 100%;
   list-style: none;
   word-break: break-word;
   cursor: pointer;
   white-space: normal;
-  color: rgba(0, 0, 0, 0.8);
   box-sizing: inherit;
   overflow: hidden;
   height: 100%;
 `;
 
 export const Image = styled.img`
-  -webkit-text-size-adjust: 100%;
-  list-style: none;
-  word-break: break-word;
-  cursor: pointer;
   white-space: normal;
-  color: rgba(0, 0, 0, 0.8);
   box-sizing: inherit;
   border: 0;
   display: block;
@@ -38,42 +30,32 @@ export const Image = styled.img`
   width: 40px;
   vertical-align: top;
   float: left;
-  background: #ffffff;
 `;
 
 export const DrugName = styled.span`
-  -webkit-text-size-adjust: 100%;
-  list-style: none;
-  word-break: break-word;
   box-sizing: inherit;
   position: relative;
   border: 2px solid transparent;
   outline: 0;
   display: block;
-  cursor: pointer;
   padding: 0 0 0 10px;
-  background: none;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-  color: rgba(0, 0, 0, 0.8);
 `;
 
 export const Brand = styled.span`
   -webkit-text-size-adjust: 100%;
   font-weight: 300;
-  list-style: none;
   word-break: break-word;
   box-sizing: inherit;
   position: relative;
   border: 2px solid transparent;
   outline: 0;
   display: block;
-  cursor: pointer;
   padding: 0 0 0 10px;
   background: none;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-  color: rgba(0, 0, 0, 0.8);
 `;
